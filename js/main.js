88d9780a0e67a1fc49d3a864c64399b056570896
Jean-CODE-Van-Damme <oreverseau@live.fr>
2022-06-19 14:58:31 +0200

    back deployed 2

--- js/main.js
+++ js/main.js
@@ -2,23 +2,49 @@
 
 document.addEventListener("DOMContentLoaded", () => {
   console.log("chargé");
-});
 
-// detecter le clic nimp ou sur la page
+  //afficher le texte en supprimant la classe
 
-//   Détecter le clique n'importe où sur la page
-// document.addEventListener("click", () => {
-//   console.log("cliqué!");
-// });
+  document.querySelector("#afficher").addEventListener("click", () => {
+    console.log("hello");
+    document.querySelector("#text").classList.remove("hidden");
+  });
 
-// clic sur le boutton faire apparraitre le texte
+  //enlever le texte en remetant la classe
 
-// document.querySelector(`#afficher_cacher`).addEventListener("click", () => {
-//   document.querySelector(`# text`).classList.remove(`.hidden`);
+  document.querySelector("#cacher").addEventListener("click", () => {
+    document.querySelector("#text").classList.add("hidden");
+  });
 
-//   console.log("goodclick");
-// });
+  // enlever la classer ou reafficher la classe
 
-document.getElementById("afficher_cacher").addEventListener("click", () => {
-  document.getElementById("text").classList.remove("hidden");
+  document.querySelector("#afficher_cacher").addEventListener("click", () => {
+    document.querySelector("#text").classList.toggle("hidden");
+  });
+
+  // annule le comportement par défaut de rafraichissement
+
+  document.querySelector("#form").addEventListener("submit", async (event) => {
+    event.preventDefault(); // annule le comportement par défaut de rafraichissement
+    console.log("submit");
+
+    // recuperer les elements envoyer des  differents #id
+    // vers le back soit en local host
+    // http://localhost:3000/form"
+
+    // soit en serveur heberge sur heroku
+    // https://form-back-jcvd.herokuapp.com/form
+
+    const response = await axios.post(
+      "https://form-back-jcvd.herokuapp.com/form",
+      {
+        firstname: document.querySelector("#firstname").value,
+        lastname: document.querySelector("#lastname").value,
+        email: document.querySelector("#email").value,
+        message: document.querySelector("#message").value,
+      }
+    );
+
+    console.log("response>>>", response);
+  });
 });
